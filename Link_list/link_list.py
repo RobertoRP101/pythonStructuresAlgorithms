10ed4e484e312004b316e1263f7b13dee4011611
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-09 23:48:28 -0600

    linkedlist: creation defined

--- Link_list/link_list.py
+++ Link_list/link_list.py
@@ -3,3 +3,9 @@ class Node(object):
         self.value = value
         self.next = None        
 
+class LinkedList(object):
+    def __init__(self, value) -> None:
+        new_node = Node(value)
+        self.head = new_node 
+        self.tail = new_node
+        self.length = 1 

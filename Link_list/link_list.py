de99e13ef299bbcb74e997805223b8143a328d81
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-11 23:41:08 -0600

    linkedlist: append defined, adding element if there is at least one element in the linklist

--- Link_list/link_list.py
+++ Link_list/link_list.py
@@ -14,7 +14,12 @@ class LinkedList(object):
         new_node = Node(value)
         if self.head is None:
             self.head = new_node    
-            self.tail = new_node    
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
         
     def print_list(self):
         temp = self.head

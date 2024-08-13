4efb104006e58afa3f58e5cd25484799df7ca6a3
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-12 23:34:02 -0600

    linkedlist: prepend defined, handle a linklist when it has at least one element

--- Link_list/link_list.py
+++ Link_list/link_list.py
@@ -43,9 +43,14 @@ class LinkedList(object):
             self.tail = None
         return temp.value
     
-    def append(self, value):
+    def prepend(self, value):
         new_node = Node(value)
         if self.length == 0:
             self.head = new_node
             self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
             

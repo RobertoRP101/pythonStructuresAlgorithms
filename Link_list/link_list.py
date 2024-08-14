04059c8737fa15fb9dc7569061ebb1070ae09a76
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-13 23:10:47 -0600

    linkedlist: pop_first defined, handle a linklist when it has at least two elements

--- Link_list/link_list.py
+++ Link_list/link_list.py
@@ -57,5 +57,9 @@ class LinkedList(object):
     def pop_first(self):
         if self.length == 0:
             return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
         
         

a74c57d664e8d75a573dbacc09b7fc35698d2ffc
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-17 23:54:19 -0600

    doublylinkedlist: node class defined

--- Doubly_linked_list/doubly_linked_list.py
+++ Doubly_linked_list/doubly_linked_list.py
@@ -1,3 +1,8 @@
+class Node(object):
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None   
+
 class DoublyLinkedList:
     def __init__(self, value):
         pass

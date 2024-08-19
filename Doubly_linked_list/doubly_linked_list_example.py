28cb4936709479a1174a5b051e98358ce4e125d0
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-18 23:33:58 -0600

    doublylinkedlist: append defined, test added

--- Doubly_linked_list/doubly_linked_list_example.py
+++ Doubly_linked_list/doubly_linked_list_example.py
@@ -2,3 +2,6 @@ from doubly_linked_list import DoublyLinkedList
 
 my_doubly_linked_list = DoublyLinkedList(3)
 my_doubly_linked_list.print_list()
+my_doubly_linked_list.append(4)
+my_doubly_linked_list.append(10)
+my_doubly_linked_list.print_list()

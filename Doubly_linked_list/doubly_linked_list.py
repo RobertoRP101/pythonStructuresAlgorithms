24285b961b7d47d7ddd8f8c7886b51ce508ba0fc
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-18 23:17:44 -0600

    doublylinkedlist: append defined, handle at least one item in the doublylinkedlist

--- Doubly_linked_list/doubly_linked_list.py
+++ Doubly_linked_list/doubly_linked_list.py
@@ -22,3 +22,7 @@ class DoublyLinkedList:
         if self.head is None:
             self.head = new_node
             self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node

27b2d776c75c3e48cf9c7ec776695b22127e1870
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-23 23:48:23 -0600

    doublylinkedlist: remove defined, handle if the index is between the first and last element

--- Doubly_linked_list/doubly_linked_list.py
+++ Doubly_linked_list/doubly_linked_list.py
@@ -116,7 +116,15 @@ class DoublyLinkedList:
             return self.pop_first()
         if index == self.length - 1:
             return self.pop()
-    
+        
+        temp = self.get(index)
+        temp.next.prev = temp.prev
+        temp.prev.next = temp.next
+        temp.next = None
+        temp.prev = None
+        
+        self.length -= 1
+        return temp     
         
 
             

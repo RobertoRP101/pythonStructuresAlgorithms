db2364e4a7ed7d3430b0c2623f4151987338ea18
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-24 23:56:01 -0600

    stack: push defined, handle if the stack has at least one element

--- Stack/stack.py
+++ Stack/stack.py
@@ -20,3 +20,8 @@ class Stack(object):
         new_node = Node(value)
         if self.height == 0:
             self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+        

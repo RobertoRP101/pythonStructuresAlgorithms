4f72059f374981f572acf7e078b07eb81599086e
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-27 14:00:15 -0600

    queue: dequeue defined, handle if the stack has one element

--- Queue/queue.py
+++ Queue/queue.py
@@ -30,3 +30,7 @@ class Queue:
     def dequeue(self):
         if self.length == 0:
             return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None

d183ec9a4db53d6fbead3c132b95248d36037ac6
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-26 23:46:08 -0600

    queue: constructor defined

--- Queue/queue.py
+++ Queue/queue.py
@@ -2,3 +2,11 @@ class Node(object):
     def __init__(self, value) -> None:
         self.value = value
         self.next = None   
+        
+        
+class Queue:
+    def __init__(self, value) -> None:
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1

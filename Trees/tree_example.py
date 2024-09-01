f7277134b37e5b0ceefbb33cd5d57805c7084aa6
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-08-31 23:10:48 -0600

    tress: insert defined, test added

---
+++ Trees/tree_example.py
@@ -0,0 +1,10 @@
+from tree import BinarySearchTree
+
+my_tree = BinarySearchTree()
+my_tree.insert(2)
+my_tree.insert(1)
+my_tree.insert(3)
+
+print(my_tree.root.value)
+print(my_tree.root.left.value)
+print(my_tree.root.right.value)

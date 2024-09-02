2bfc93f8d39ad6875df40590cb6594a7fae3cb00
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-09-01 23:00:39 -0600

    hashtable: __hash defined, application of hash operations

--- Hash_Table/hashtable.py
+++ Hash_Table/hashtable.py
@@ -1,3 +1,9 @@
 class HashTable(object):
     def __init__(self, size=7) -> None:
         self.data_map = [None] * size
+    
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash

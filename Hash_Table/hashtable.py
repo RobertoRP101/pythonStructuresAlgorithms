fbc0f9d28e765a829bf462fb50d99c4ce969fef4
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-09-02 04:42:09 -0600

    hashtable: get_item defined, handle if the row of the table is not empty and try to return the value

--- Hash_Table/hashtable.py
+++ Hash_Table/hashtable.py
@@ -19,4 +19,8 @@ class HashTable(object):
         self.data_map[index].append([key, value])
         
     def get_item(self, key):
-        pass
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]

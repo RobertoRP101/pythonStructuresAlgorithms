810af8097606aac3a7a9d0044567d98dfba03728
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-09-03 19:31:55 -0600

    hashtable: keys defined, multiple tests added

--- Hash_Table/hashtable_example.py
+++ Hash_Table/hashtable_example.py
@@ -4,6 +4,8 @@ my_table = HashTable()
 
 my_table.print_table()
 my_table.set_item('Running', 1000)
+my_table.set_item('Walking', 200)
+my_table.set_item('Bike', 800)
 my_table.print_table()
 print(my_table.get_item('Running'))
 print(my_table.keys())

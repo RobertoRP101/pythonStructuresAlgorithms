16169cd01ea3f945fc6f486f5ea9962bad6e9995
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-09-03 19:30:02 -0600

    hashtable: keys defined, get all the keys from the hashtable

--- Hash_Table/hashtable.py
+++ Hash_Table/hashtable.py
@@ -25,3 +25,12 @@ class HashTable(object):
                 if self.data_map[index][i][0] == key:
                     return self.data_map[index][i][1]
         return None
+    
+    
+    def keys(self):
+        all_keys = []
+        for i in range(len( self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys

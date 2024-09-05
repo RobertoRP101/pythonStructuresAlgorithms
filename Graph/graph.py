0c5017655097abee55e21055d834db1d87ab4a3d
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-09-05 17:58:46 -0600

    graph: print_graph added, display all the elements from graph

--- Graph/graph.py
+++ Graph/graph.py
@@ -1,3 +1,7 @@
 class Graph(object):
     def __init__(self):
         self.adj_list = {}
+        
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ':', self.adj_list[vertex])

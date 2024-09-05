958d69dda9368d250ac4ba9967e75d5b0c8a4876
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-09-04 18:05:22 -0600

    interview_question: lineal solution

--- Hash_Table/question.py
+++ Hash_Table/question.py
@@ -2,12 +2,20 @@ list1 = [1,2,3,4,5,10]
 list2 = [10,9,8,7,6,5,4]
 
 
-def item_in_commun(list1, list2):
+# def item_in_common(list1, list2):
+#     for i in list1:
+#         for j in list2:
+#             if i == j:
+#                 return True
+#     return False
+
+def item_in_common(list1, list2):
+    dict1 = {}
     for i in list1:
-        for j in list2:
-            if i == j:
-                return True
+        dict1[i] = True
+    
+    for j in list2:
+        if j in dict1:
+            return True
     return False
-
-
 

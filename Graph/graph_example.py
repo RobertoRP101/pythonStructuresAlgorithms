4497c4e18edec5c47a4d09963c5ce46b03f96a93
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-09-05 18:04:56 -0600

    graph: multiple test added

--- Graph/graph_example.py
+++ Graph/graph_example.py
@@ -2,4 +2,11 @@ from graph import Graph
 
 my_graph = Graph()
 my_graph.add_vertex('A')
+my_graph.add_vertex('B')
+my_graph.add_vertex('C')
+my_graph.add_vertex('D')
+my_graph.add_vertex('E')
+my_graph.print_graph()
+my_graph.add_vertex('D')
+my_graph.add_vertex('F')
 my_graph.print_graph()

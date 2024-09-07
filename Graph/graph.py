1d4c7e454154c8b344c63d9f8262c1748139f5e0
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-09-06 22:07:45 -0600

    graph: add_edge defined, a little tweaking

--- Graph/graph.py
+++ Graph/graph.py
@@ -16,4 +16,5 @@ class Graph(object):
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
             self.adj_list[v1].append(v2)
             self.adj_list[v2].append(v1)
-            
+            return True
+        return False

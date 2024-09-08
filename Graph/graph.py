d73df5998ac87b5f97a9cb1394556a79ea635ded
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-09-07 20:33:19 -0600

    graph: remove_edge defined, a little twaking

--- Graph/graph.py
+++ Graph/graph.py
@@ -23,3 +23,5 @@ class Graph(object):
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
             self.adj_list[v1].remove(v2)
             self.adj_list[v2].remove(v1)
+            return True
+        return False

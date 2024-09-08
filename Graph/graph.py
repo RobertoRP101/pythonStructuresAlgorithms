f24eba4cf56c082a205d05e02117232a59bf550e
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-09-07 20:32:46 -0600

    graph: remove_edge defined, remove the connection between them

--- Graph/graph.py
+++ Graph/graph.py
@@ -21,4 +21,5 @@ class Graph(object):
     
     def remove_edge(self, v1, v2):
         if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
-            pass
+            self.adj_list[v1].remove(v2)
+            self.adj_list[v2].remove(v1)

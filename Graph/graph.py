a9ce02a122b19803bfb3b50b0beb6c15dff43667
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-09-08 22:41:13 -0600

    graph: remove_vertex defined, delete the vertex

--- Graph/graph.py
+++ Graph/graph.py
@@ -30,3 +30,4 @@ class Graph(object):
         if vertex in self.adj_list.keys():
             for other_vertex in self.adj_list[vertex]:
                 self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]

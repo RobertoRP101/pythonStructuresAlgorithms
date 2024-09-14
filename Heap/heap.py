86fafc4b014c314e412a6055c156f1e8249a68e2
Robert'RP <roberto.riveros.padilla101@gmail.com>
2024-09-13 23:56:53 -0600

    heap: remove defined, tweaks

--- Heap/heap.py
+++ Heap/heap.py
@@ -27,3 +27,9 @@ class MaxHead(object):
         
         if len(self.heap) == 1:
             return self.heap.pop()
+        
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+        
+        return max_value
